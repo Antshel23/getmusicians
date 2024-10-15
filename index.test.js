3ad682adb82c171f278630eeea60ffe0f7c18ee8
Antshel23 <antsheldon23@icloud.com>
2024-10-15 10:02:33 +0100

    get request and associated test complete

--- index.test.js
+++ index.test.js
@@ -1,4 +1,3 @@
-// install dependencies
 const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
@@ -11,12 +10,22 @@ const {seedMusician} = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
+    test("Testing endpoint existence", async () => {
+        // get request for endpoint validity
+        const response = await request(app).get("/musicians");
+    })
+    test("Testing endpoint response status", async () => {
+        // get request for endpoint success status
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
+    test("Testing endpoint response contents", async () => {
+        // get request, parsing response from HTML -> JSON, expecting for each musician instance to equal seed data
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        responseData.forEach((musician, index) => {
+            expect(musician.name).toEqual(seedMusician[index].name);
+            expect(musician.instrument).toEqual(seedMusician[index].instrument);
+    })
+})
 })
